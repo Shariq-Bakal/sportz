1540ad2628d6f3a0403ab2235abd9bd421c29302
Shariq <shariq.f.bakal@gmail.com>
2026-06-29 22:12:58 +0530

    implemented POST AND GET matches routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { matchesRouter } from "./routes/matches.js";
 const app = express();
 const port = 8080;
 
@@ -9,6 +10,7 @@ app.get("/",(req,res)=>{
     res.send("This is a home page")
 })
 
+app.use("/matches", matchesRouter)
 
 app.listen(port,()=>{
     console.log(`App is listening on port http://localhost:${port}`)
